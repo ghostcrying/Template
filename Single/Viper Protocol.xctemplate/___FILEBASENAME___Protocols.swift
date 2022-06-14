d14ab38777b984fa40e2640ad42d248d9bf7772c
Joy <chenzhuo@bloks.com>
2022-06-14 09:49:07 +0800

    - Optimize protocol

--- Single/Viper Protocol.xctemplate/___FILEBASENAME___Protocols.swift
+++ Single/Viper Protocol.xctemplate/___FILEBASENAME___Protocols.swift
@@ -14,8 +14,25 @@ protocol ___VARIABLE_productName:identifier___WireframeProtocol: AnyObject {
 }
 
 // MARK: Presenter -
-protocol ___VARIABLE_productName:identifier___PresenterProtocol: InheritControllerPresenterProtocol {
+protocol ___VARIABLE_productName:identifier___PresenterProtocol: AnyObject {
     
+    func viewDidLoad()
+    func viewWillAppear(_ animated: Bool)
+    func viewDidAppear(_ animated: Bool)w
+    func viewWillDisappear(_ animated: Bool)
+    func viewDidDisappear(_ animated: Bool)
+    func viewWillLayoutSubviews()
+    func viewDidLayoutSubviews()
+}
+extension ___VARIABLE_productName:identifier___PresenterProtocol {
+    
+    func viewDidLoad() {}
+    func viewWillAppear(_ animated: Bool) {}
+    func viewDidAppear(_ animated: Bool) {}
+    func viewWillDisappear(_ animated: Bool) {}
+    func viewDidDisappear(_ animated: Bool) {}
+    func viewWillLayoutSubviews() {}
+    func viewDidLayoutSubviews() {}
 }
 
 // MARK: Interactor -
